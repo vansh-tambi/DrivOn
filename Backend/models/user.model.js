f081d3a7ec3f84fada20e2d1c1b4e356092d2b79
vansh-tambi <23u02084@iiitbhopal.ac.in>
2025-03-23 15:58:53 +0530

    user model updated

---
+++ Backend/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        firstName:{
+            type: String,
+            required: true,
+            minlength: [3, 'First Name Must be at least 3 characters long']
+        },
+        lastName:{
+            type: String,
+            minlength: [3, 'Last Name Must be at least 3 characters long']
+        }
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId:{
+        type:String
+    }
+})
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
