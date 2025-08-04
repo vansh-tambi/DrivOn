3b3e79bf31f9b3e7fb5d0a730dd331a6c5904a59
vansh-tambi <23u02084@iiitbhopal.ac.in>
2025-08-04 10:48:54 +0530

    .

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -24,11 +24,7 @@ const CaptainSignup = () => {
 
 
   const verifyEmail = async (email) => {
-<<<<<<< HEAD
     const apiKey = 'eda82a106d044c8b88e883cedbdcebad'; // replace with your real key
-=======
-    const apiKey = '7c1c70e2185c53db58ba3cf98791d834e7553d1e'; // replace with your real key
->>>>>>> d11aaebc8f0a73d976fc711410e3553d267796fd
     const url = `https://api.zerobounce.net/v2/validate?api_key=${apiKey}&email=${email}`;
   
     try {
